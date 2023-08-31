cb00b9006a67ffb4717a0bec02ea77ebdffd935e
anthoni-pivoto <anthonipivoto@gmail.com>
2023-08-31 12:00:30 -0300

    Português:
    Alterando arquivo README e familiarizando com Java
    
    English:
    
    Updating README archive and getting used to Java

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Santander_Bootcamp
+
+## Português:
+
+Repositório para armazenar o conteúdo do curso de Java
+
+## English
+
+Repository to store Java couse content
---
+++ primeiro_projeto_java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá pessoas que vao avaliar meus projetos");
+    }
+}
